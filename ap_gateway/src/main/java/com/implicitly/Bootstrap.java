e1d74a6d1cb64329dcf86ce8a8f596b6b2b70d5e
implicitly <murzakaev.emil.it@gmail.com>
2020-04-15 11:13:14 +0300

    banner mode

--- ap_gateway/src/main/java/com/implicitly/Bootstrap.java
+++ ap_gateway/src/main/java/com/implicitly/Bootstrap.java
@@ -1,8 +1,7 @@
 package com.implicitly;
 
-import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -15,10 +14,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class Bootstrap {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .bannerMode(Banner.Mode.OFF)
-                .sources(Bootstrap.class)
-                .run(args);
+        SpringApplication.run(Bootstrap.class, args);
     }
 
 }
